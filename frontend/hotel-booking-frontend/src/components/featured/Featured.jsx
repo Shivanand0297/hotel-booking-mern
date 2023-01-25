4aa4c2e0cd98df4f4b4d27ece1729bbb74b08c85
Shivanand <Shivanand0297@gmail.com>
2023-01-25 17:52:50 +0530

    done with featuredProperties components

--- frontend/hotel-booking-frontend/src/components/featured/Featured.jsx
+++ frontend/hotel-booking-frontend/src/components/featured/Featured.jsx
@@ -19,14 +19,14 @@ const Featured = () => {
             </div>
         </div>
         <div class="featuredItem">
-            <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/city/600x600/684730.jpg?k=e37b93d88c1fe12e827f10c9d6909a1def7349be2c68df5de885deaa4bc01ee3&o="/>
+            <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/city/600x600/684730.jpg?k=e37b93d88c1fe12e827f10c9d6909a1def7349be2c68df5de885deaa4bc01ee3&o=" alt="Channai" />
             <div class="featuredTitle">
                 <h1>Channai</h1>
                 <h2>60k+ Properties</h2>
             </div>
         </div>
         <div class="featuredItem">
-            <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/city/600x600/684765.jpg?k=3f7d20034c13ac7686520ac1ccf1621337a1e59860abfd9cbd96f8d66b4fc138&o=" alt="New delhi"/>
+            <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/city/600x600/684765.jpg?k=3f7d20034c13ac7686520ac1ccf1621337a1e59860abfd9cbd96f8d66b4fc138&o=" alt="Jaipur"/>
             <div class="featuredTitle">
                 <h1>Jaipur</h1>
                 <h2>80k+ Properties</h2>
--- frontend/hotel-booking-frontend/src/pages/home/Home.jsx
+++ frontend/hotel-booking-frontend/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import PropertyList from "../../components/propertylist/PropertyList"
@@ -14,6 +15,8 @@ const Home = () => {
         <Featured/>
         <h1 className='homeTitle' >Browse By Property Type</h1>
         <PropertyList/>
+        <h1 className='homeTitle' >Home Guest Love</h1>
+        <FeaturedProperties/>
       </div>
     </div>
   )
