20bfbfa8e318ee245fc97d4e698f5a2fe8816d54
Shivanand <Shivanand0297@gmail.com>
2023-01-29 17:16:05 +0530

    login request to backend

--- frontend/hotel-booking-frontend/src/components/navbar/Navbar.jsx
+++ frontend/hotel-booking-frontend/src/components/navbar/Navbar.jsx
@@ -1,14 +1,30 @@
+import axios from "axios";
 import React from "react";
 import "./navbar.css"
 
 const Navbar = () => {
+
+  const login = async () =>{
+
+
+    const userData = {
+      email: "admin@gmail.com",
+      password: "admin",
+    }
+
+    const response = await axios.post("/api/v1/auth/login", userData, {
+        credentials: "include"
+      })
+      console.log(response.data);
+  }
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <span className="logo">Booking</span>
         <div className="navItems">
           <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
+          <button className="navButton" onClick={login} >Login</button>
         </div>
       </div>
     </div>
