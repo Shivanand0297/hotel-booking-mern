d1adc5892386054f6aa53d057e3801182c7d2943
Shivanand <Shivanand0297@gmail.com>
2023-02-09 19:49:47 +0530

    added hosted frontend url

--- backend/hotel-booking-backend/index.js
+++ backend/hotel-booking-backend/index.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(
   cors({
-      origin: "http://localhost:3000",
+      origin: "https://shivanand-hotel-booking.netlify.app",
       credentials: true
   })
 );
