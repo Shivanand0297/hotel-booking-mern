2e6ce0ded0d6253621f3bb3208a71e0b7608f940
Shivanand <Shivanand0297@gmail.com>
2023-02-10 21:12:35 +0530

    sameSite to Lax

--- backend/hotel-booking-backend/controllers/auth.controller.js
+++ backend/hotel-booking-backend/controllers/auth.controller.js
@@ -89,7 +89,8 @@ export const login = async (req, res, next)=>{
             // setting cookie for 3 days
             res.cookie("authToken", token, {
                 expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-                httpOnly: true 
+                httpOnly: true,
+                sameSite: lax 
             })
 
             // removing password before sending the user
