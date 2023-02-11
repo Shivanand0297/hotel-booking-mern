641648d864589528d308cfa70625b7d44367f3d0
Shivanand <Shivanand0297@gmail.com>
2023-02-12 00:18:19 +0530

    struggling with the cookies

--- backend/hotel-booking-backend/index.js
+++ backend/hotel-booking-backend/index.js
@@ -43,7 +43,7 @@ app.use(cookieParser())
 app.use(
     cors({
       origin: ["https://shivanand-hotel-booking.netlify.app", 
-      "https://shivanand-hotel-booking.vercel.app/"],
+      "https://shivanand-hotel-booking.vercel.app"],
       credentials: true
     })
 );
