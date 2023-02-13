bf63761817732cab88cf5f3b3bcae69d40ad22ea
Shivanand <Shivanand0297@gmail.com>
2023-02-13 12:24:00 +0530

    removed console.log

--- backend/hotel-booking-backend/middleware/verifyToken.js
+++ backend/hotel-booking-backend/middleware/verifyToken.js
@@ -11,9 +11,6 @@ export const verifyToken = async (req, _res, next)=>{
         req.cookies.authToken || (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) 
         ){
             token = req.cookies.authToken || req.headers.authorization.split(" ")[1]
-            // console.log(req.cookies)
-            console.log(req.headers.authorization);
-            console.log(token);
         }
         
         if(!token) return next(errorHandler(401, "You are not authenticated"))
